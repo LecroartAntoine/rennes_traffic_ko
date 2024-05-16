39dbd75f2a7ce10ae096d9067c339518e86fb10e
Lécroart Antoine <115992298+LecroartAntoine@users.noreply.github.com>
2024-05-16 16:59:48 +0200

    Initial commit
    - Fixed several bugs
    - added requirements

--- src/get_data.py
+++ src/get_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 
+
 class GetData(object):
 
     def __init__(self, url) -> None:
@@ -10,12 +11,31 @@ class GetData(object):
         self.data = response.json()
 
     def processing_one_point(self, data_dict: dict):
+        """_summary_
 
-        temp = pd.DataFrame({key:[data_dict[key]] for key in ['datetime', 'traffic_status', 'geo_point_2d', 'averagevehiclespeed', 'traveltime', 'trafficstatus']})
-        temp = temp.rename(columns={'traffic_status':'traffic'})
-        temp['lat'] = temp.geo_point_2d.map(lambda x : x['lattitude'])
-        temp['lon'] = temp.geo_point_2d.map(lambda x : x['longitude'])
-        del temp['geo_point_2d']
+        Args:
+            data_dict (dict): _description_
+
+        Returns:
+            _type_: _description_
+        """
+
+        temp = pd.DataFrame(
+            {
+                key: [data_dict[key]]
+                for key in [
+                    "datetime",
+                    "geo_point_2d",
+                    "averagevehiclespeed",
+                    "traveltime",
+                    "traffic_status",
+                ]
+            }
+        )
+        temp = temp.rename(columns={"trafficstatus": "traffic"})
+        temp["lat"] = temp.geo_point_2d.map(lambda x: x["lat"])
+        temp["lon"] = temp.geo_point_2d.map(lambda x: x["lon"])
+        del temp["geo_point_2d"]
 
         return temp
 
@@ -24,9 +44,8 @@ class GetData(object):
         res_df = pd.DataFrame({})
 
         for data_dict in self.data:
-        temp_df = self.processing_one_point(data_dict)
-        res_df = pd.concat([res_df, temp_df])
-
-        res_df = res_df[res_df.traffic != 'unknown'
+            temp_df = self.processing_one_point(data_dict)
+            res_df = pd.concat([res_df, temp_df])
+            res_df = res_df[res_df.traffic != "unknown"]
 
         return res_df
--- src/utils.py
+++ src/utils.py
@@ -2,26 +2,43 @@ import plotly.express as px
 import numpy as np
 
 
-
 def create_figure(data):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
 
     fig_map = px.scatter_mapbox(
-            data,
-            title="Traffic en temps réel",
-            color="traffic",
-            lat="lat",
-            lon="lon",
-            color_discrete_map={'freeFlow':'green', 'heavy':'orange', 'congested':'red'},
-            zoom=10
-            height=500,
-            mapbox_style="carto-positron"
+        data,
+        title="Traffic en temps réel",
+        color="traffic",
+        lat="lat",
+        lon="lon",
+        color_discrete_map={"freeFlow": "green", "heavy": "orange", "congested": "red"},
+        zoom=10,
+        height=500,
+        mapbox_style="carto-positron",
     )
 
     return fig_map
 
-def prediction_from_model(model, hour_to_predict):
 
-    input_pred = np.array([0]*25)
+def prediction_from_model(model, hour_to_predict):
+    """_summary_
+
+    Args:
+        model (_type_): _description_
+        hour_to_predict (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    input_pred = np.array([0] * 24)
     input_pred[int(hour_to_predict)] = 1
 
     cat_predict = np.argmax(model.predict(np.array([input_pred])))
